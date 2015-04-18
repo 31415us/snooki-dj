b1e352940e4a2a6e9c0c81be1d99380ad201dc97
Dennis van der Bij <d.vanderbij@gmail.com>
2015-04-18 23:03:30 +0200

    Remove redundencies.

--- snooki/chord_extractor.py
+++ snooki/chord_extractor.py
@@ -58,7 +58,19 @@ def get_longest_progression(f):
             if len(p) > max:
                 max_progression = p
 
-        return p
+        prev_progression = max_progression[0]
+        count = max_progression[1]
+        progression = []
+
+        for p in max_progression[1:]:
+            if prev_progression[0] == p[0]:
+                count += p[1]
+            else:
+                progression.append((prev_progress[0], count))
+                prev_progression = p
+                count = p[1]
+
+        return progression
     
     except:
         return []
