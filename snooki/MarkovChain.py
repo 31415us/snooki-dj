041e819e32527aca79830abeec6b1cbb2ebd9ff0
Pius von Däniken <pius.vondaeniken@epfl.ch>
2015-04-18 21:28:00 +0200

    markov chain simulate

--- snooki/MarkovChain.py
+++ snooki/MarkovChain.py
@@ -1,13 +1,14 @@
 
 from collections import defaultdict
+from random import random
 
 class MarkovChain(object):
 
     START = "start"
 
     def __init__(self, sequences):
-        # laplace smoothing similar to naive bayes
-        self.bigram_count = defaultdict(lambda: 1)
+        self.bigram_count = defaultdict(int)
+        self.neighbours = defaultdict(set)
 
         for seq in sequences:
             self._seq_to_bigrams(seq)
@@ -18,4 +19,43 @@ class MarkovChain(object):
 
         for (chord, repetitions) in seq:
             self.bigram_count[(prev, chord)] += 1
+            self.neighbours[prev].add(chord)
             prev = chord
+
+    def _next_state(self, state):
+
+        weights = 0
+
+        probas = {}
+
+        for n in self.neighbours[state]:
+            b_count = self.bigram_count[(state, n)]
+            weights += b_count
+            probas[n] = b_count
+
+        for n in probas:
+            proba[n] /= weights
+
+        r = random()
+
+        acc = 0
+
+        for n in probas:
+            acc += probas[n]
+            if acc < r:
+                return n
+
+        return state
+
+    def create_progression(self, length):
+        current_state = self.START
+        
+        for i in range(0, length):
+            current_state = self._next_state(current_state)
+            yield current_state
+
+    def infinite_progression(self):
+        current_state = self.START
+        while True:
+            current_state = self._next_state(current_state)
+            yield current_state
