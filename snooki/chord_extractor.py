805ea4396857b4772f88e2a304369db50cedb233
Pius von Däniken <pius.vondaeniken@epfl.ch>
2015-04-18 21:52:51 +0200

    ugly fix chord_extractor

--- snooki/chord_extractor.py
+++ snooki/chord_extractor.py
@@ -2,6 +2,8 @@ from mingus.midi.MidiFileIn import MIDI_to_Composition
 from mingus.containers.NoteContainer import NoteContainer
 from mingus.midi import fluidsynth
 
+import os
+
 
 def get_composition(file):
     try:
@@ -12,56 +14,62 @@ def get_composition(file):
 
 
 def get_longest_progression(f):
-    (comp, bpm) = get_composition(f)
-    progression = []
-    progressions = []
-    curr_chord = None
-    chord_length = 1
+    try:
+        (comp, bpm) = get_composition(f)
 
-    for track in comp:
-        for bar in track:
-            for cont in bar:
-                notes = cont[2]
+        progression = []
+        progressions = []
+        curr_chord = None
+        chord_length = 1
 
-                if len(notes) > 2:
-                    if curr_chord is not None:
-                        progression.append((curr_chord, chord_length))
-                        curr_chord = NoteContainer(notes).determine()
+        for track in comp:
+            for bar in track:
+                for cont in bar:
+                    notes = cont[2]
 
-                        if len(curr_chord) != 0:
-                            curr_chord = curr_chord[0]
+                    if len(notes) > 2:
+                        if curr_chord is not None:
+                            progression.append((curr_chord, chord_length))
+                            curr_chord = NoteContainer(notes).determine()
+
+                            if len(curr_chord) != 0:
+                                curr_chord = curr_chord[0]
+                            else:
+                                curr_chord = None
+                                break
+
+                            chord_length = 1
                         else:
-                            curr_chord = None
-                            break
+                            curr_chord = NoteContainer(notes).determine()
 
-                        chord_length = 1
+                            if len(curr_chord) != 0:
+                                curr_chord = curr_chord[0]
+                            else:
+                                curr_chord = None
+                                break
                     else:
-                        curr_chord = NoteContainer(notes).determine()
+                        chord_length += 1
 
-                        if len(curr_chord) != 0:
-                            curr_chord = curr_chord[0]
-                        else:
-                            curr_chord = None
-                            break
-                else:
-                    chord_length += 1
+            progressions.append(progression)
 
-        progressions.append(progression)
+        max = 0;
+        max_progression = None
+        for p in progressions:
+            if len(p) > max:
+                max_progression = p
 
-    max = 0;
-    max_progression = None
-    for p in progressions:
-        if len(p) > max:
-            max_progression = p
-
-    return p
+        return p
+    
+    except:
+        return []
 
 
 def get_all_progressions():
     progressions = []
 
     with open("../midi/dance/paths.txt") as f:
-        progressions.append(get_longest_progression("../midi/" + f.readline()))
+        for line in f:
+            progressions.append(get_longest_progression(os.path.normpath("../midi/" + line.strip())))
 
     return progressions
 
