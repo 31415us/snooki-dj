f1eb03dd2c831a38904f7443c0028b22106d6465
Pius von Däniken <pius.vondaeniken@epfl.ch>
2015-04-18 17:48:00 +0200

    test fluidsynth

--- snooki/chord_extractor.py
+++ snooki/chord_extractor.py
@@ -1,13 +1,19 @@
 from mingus.midi.MidiFileIn import MIDI_to_Composition
 
+from mingus.midi import fluidsynth
+
 
 def get_composition(file):
     try:
-        MIDI_to_Composition(file)
+        return MIDI_to_Composition(file)
     except Exception as e:
         print (e)
         print ("Could not load midi file: " + file)
 
 
 file = "../midi/dance/darude/Sandstorm.mid"
-get_composition(file)
+(comp, bpm) = get_composition(file)
+
+fluidsynth.init("../soundfonts/soundfont.sf2", "alsa")
+
+fluidsynth.play_Composition(comp, None, bpm)
