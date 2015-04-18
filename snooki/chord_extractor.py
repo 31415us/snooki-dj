68b36a461e5d2ae2632cb3f62655451856561724
Dennis van der Bij <dennis.vanderbij@epfl.ch>
2015-04-18 16:04:18 +0200

    Try to read midi file.

--- snooki/chord_extractor.py
+++ snooki/chord_extractor.py
@@ -1,6 +1,13 @@
-import midi
+from mingus.midi.MidiFileIn import MIDI_to_Composition
 
-def get_pattern(file):
-    return midi.read_midfile(file)
 
-print get_pattern("midi/dance/darude/Sandstorm.mid")
+def get_composition(file):
+    try:
+        MIDI_to_Composition(file)
+    except Exception as e:
+        print (e)
+        print ("Could not load midi file: " + file)
+
+
+file = "./midi/dance/darude/Sandstorm.mid"
+get_composition(file)
